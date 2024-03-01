805dbf3532b0210083461e1e37dc4d9c15b78aad
Nikesh Bhandari <bhandarinikesh93@gmail.com>
2024-03-01 19:27:52 +0545

    jwt

--- WalletWatch/WalletWatch/src/main/java/com/example/WalletWatch/service/AuthenticateService.java
+++
@@ -1,9 +0,0 @@
-package com.example.WalletWatch.service;
-
-import com.example.WalletWatch.pojo.AuthenticateRequest;
-import com.example.WalletWatch.pojo.AuthenticateResponse;
-
-public interface AuthenticateService {
-
-    AuthenticateResponse authenticate(AuthenticateRequest authenticateRequest);
-}
